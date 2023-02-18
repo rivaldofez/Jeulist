d72e8ed5dc04ccfb1df3e8f7c6f273f9dae2b583
rivaldofez <rivaldofez@gmail.com>
2023-02-18 21:37:44 +0700

    add home view controller

---
+++ Jeulist/Module/Home/View/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Jeulist
+//
+//  Created by Rivaldo Fernandes on 18/02/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
