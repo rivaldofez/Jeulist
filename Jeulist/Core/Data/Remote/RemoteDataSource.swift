7a743fa03eda7d1d44f11919acb974715767d85f
rivaldofez <rivaldofez@gmail.com>
2023-02-20 14:10:18 +0700

    add function to get game list in remote data source

--- Jeulist/Core/Data/Remote/RemoteDataSource.swift
+++ Jeulist/Core/Data/Remote/RemoteDataSource.swift
@@ -11,9 +11,30 @@ import Alamofire
 
 
 protocol RemoteDataSourceProtocol: AnyObject {
-    
+    func getGameDataPagination(page: Int) -> Observable<[GameItem]>
 }
 
 final class RemoteDataSource: NSObject {
-    
+    static let sharedInstance: RemoteDataSource = RemoteDataSource()
+}
+
+extension RemoteDataSource: RemoteDataSourceProtocol {
+    func getGameDataPagination(page: Int) -> RxSwift.Observable<[GameItem]> {
+        return Observable<[GameItem]>.create { observer in
+            if let url = URL(string: "\(Endpoints.Gets.gamePagination.url)\(page)"){
+                AF.request(url)
+                    .responseDecodable(of: GameResponse.self) { response in
+                        switch response.result {
+                        case .success(let value):
+                            observer.onNext(value.results)
+                            observer.onCompleted()
+                        case .failure:
+                            observer.onError(URLError.invalidResponse)
+                        }
+                    }
+            }
+            
+            return Disposables.create()
+        }
+    }
 }
