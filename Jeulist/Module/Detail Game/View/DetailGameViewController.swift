ab3ea93b2fe7da2a3e728669dd6e6f9137526acb
rivaldofez <rivaldofez@gmail.com>
2023-02-21 15:36:15 +0700

    add image slides collection view and cell

--- Jeulist/Module/Detail Game/View/DetailGameViewController.swift
+++ Jeulist/Module/Detail Game/View/DetailGameViewController.swift
@@ -9,10 +9,61 @@ import UIKit
 
 class DetailGameViewController: UIViewController {
 
+    
+    var imgArr = [
+        UIImage(named: "testimage"),
+        UIImage(named: "testimage2")
+    ]
+    
+    private lazy var imageSlidesCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 1
+        layout.itemSize = CGSize(width: view.frame.size.width, height: 150)
+        
+        
+        let collectionview = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionview.register(ImageSlidesCollectionViewCell.self, forCellWithReuseIdentifier: ImageSlidesCollectionViewCell.identifier)
+        collectionview.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionview
+    }()
+    
+    private let imageSlidesPageControl: UIPageControl = {
+       let pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        return pageControl
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(imageSlidesCollectionView)
+        view.addSubview(imageSlidesPageControl)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints(){
+        let imageSlidesCollectionViewConstraints = [
+            imageSlidesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageSlidesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageSlidesCollectionView.topAnchor.constraint(equalTo: view.topAnchor)
+        ]
+        
+        let imageSlidesPageControlConstraints = [
+            imageSlidesPageControl.bottomAnchor.constraint(equalTo: imageSlidesCollectionView.bottomAnchor, constant: -10),
+            imageSlidesPageControl.centerXAnchor.constraint(equalTo: imageSlidesCollectionView.centerXAnchor)
+        ]
+        
+        
+        NSLayoutConstraint.activate(imageSlidesCollectionViewConstraints)
+        NSLayoutConstraint.activate(imageSlidesPageControlConstraints)
     }
 
 }
---
+++ Jeulist/Module/Detail Game/View/ImageSlidesCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ImageSlidesCollectionViewCell.swift
+//  Jeulist
+//
+//  Created by Rivaldo Fernandes on 21/02/23.
+//
+
+import UIKit
+
+class ImageSlidesCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ImageSlidesCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
