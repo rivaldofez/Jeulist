9cf2ceb9e1711d3e890e7e4dda6a7fcec7ef1d07
rivaldofez <rivaldofez@gmail.com>
2023-02-22 20:36:52 +0700

    implement home interactor

--- Jeulist/Module/Home/Interactor/HomeInteractor.swift
+++ Jeulist/Module/Home/Interactor/HomeInteractor.swift
@@ -6,12 +6,23 @@
 //
 
 import Foundation
+import RxSwift
 
 
 protocol HomeUseCase {
-    
+    func getGameDataPagination(page: Int) -> Observable<[Game]>
 }
 
 class HomeInteractor: HomeUseCase {
+    private let repository: GameRepositoryProtocol
+    
+    required init(repository: GameRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func getGameDataPagination(page: Int) -> RxSwift.Observable<[Game]> {
+        return repository.getGameDataPagination(page: page)
+    }
+    
     
 }
