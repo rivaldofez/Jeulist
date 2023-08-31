f745ce305d32d998a00ceb3d301360d3886abf8f
rivaldofez <rivaldofez@gmail.com>
2023-08-31 20:59:11 +0700

    implement profile view controller

--- Jeulist/Module/Profile/ProfileViewController.swift
+++ Jeulist/Module/Profile/ProfileViewController.swift
@@ -7,24 +7,77 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+import UIKit
 
+protocol ProfileViewProtocol {
+    var presenter: ProfilePresenterProtocol? { get set }
+}
+
+class ProfileViewController: UIViewController, ProfileViewProtocol {
+    var presenter: ProfilePresenterProtocol?
+    
+    private let profileImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.image = UIImage(named: "profile")
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        imageview.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        imageview.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        imageview.contentMode = .scaleAspectFill
+        imageview.layer.cornerRadius = 200/2
+        imageview.layer.masksToBounds = false
+        imageview.clipsToBounds = true
+        return imageview
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Rivaldo Fernandes"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18,weight: .bold)
+        label.textColor = .label
+        
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "rivaldofez@gmail.com"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .secondaryLabel
+        
+        return label
+    }()
+    
+    private lazy var profileStackView: UIStackView = {
+        let stackview = UIStackView(arrangedSubviews: [ profileImageView, nameLabel, emailLabel])
+        stackview.axis = .vertical
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.alignment = .center
+        stackview.spacing = 16
+        return stackview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .yellow
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        title = "Profile"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.tintColor = .label
+        
+        view.addSubview(profileStackView)
+        configureConstraints()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureConstraints() {
+        let profileStackViewConstraints = [
+            profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+        ]
+        
+        NSLayoutConstraint.activate(profileStackViewConstraints)
     }
-    */
-
 }
